5ef52a2e17333a45e0bf41d3d22e0bdb303a14ec
Manuel Carvalho <43664663+AndroidOS@users.noreply.github.com>
2021-10-10 20:43:02 +1100

    Added string

--- main.s
+++ main.s
@@ -6,15 +6,27 @@
 .syntax unified
 
 
+
+
+
 main:
 	mov 	r5,#0x64
-	mov		r4,#0
+	mov		r4,text
 	mov		r3,#50
 	mov		r12,#100
 	ldrb 	r9,		[r12, r3, LSL #2]
-	B		loop
+ 	B		loop
 
 loop:
 	add		r5,r5,#1
 	add		r4,r4,#1
+
+	.ALIGN
+
+.data
+text:
+        .asciz "Hello World!"
+
+
+
 	.end
