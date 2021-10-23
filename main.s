3661821011814fa4689ed38c11f3fe1f408ad2d3
Manuel Carvalho <43664663+AndroidOS@users.noreply.github.com>
2021-10-23 18:08:58 +1100

    Clean up file

--- main.s
+++ main.s
@@ -10,46 +10,28 @@
 
 
 main:
-	mov 	r5,#0x64
-	mov		r4,hello
-	mov		r3,#50
-	mov		r12,#100
-	ldrb	r5,[r4]
-	ldrb	r6,[r3]
-	mov 	r5,0x10000000
+	mov		r4,#number
 
-	str		r12,[r5]
-	ldrb	r6,[r5]
-	;.ALIGN
-	add r5,r5,#1
-
-	mov 	r12,#20
-	str		r12,[r5]
-	ldrb	r6,[r5]
-
-	sub r5,#1
-
-	ldrb	r6,[r5]
-
-	lsr r12, r12, #2
+	mov r1,#4
 Test:
-	cmp r6,#100
+	ldrb	r5,[r4]
+	sub r1,#1
+	add r4,#1
+	cmp r1,#0
 	bne	Test
  	B	loop
 
 loop:
-	add		r5,r5,#1
-	add		r4,r4,#1
+	B loop
 
-	.ALIGN
 
 .data
 ;text:
 number:
 	.byte 11,20
 
+	.align
 
-       ; .asciz "Hello World!"
 hello:
 	.asciz	"Hello"
 
